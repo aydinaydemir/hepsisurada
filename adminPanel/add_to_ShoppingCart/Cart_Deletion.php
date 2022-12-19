7a4043d2cb9e395e12e1867ecfc4c97cc4237e13
hiDeniz <halilibrahimdeniz2001@gmail.com>
2022-12-19 16:03:00 +0300

    <deleteCartGuncelleme>

--- adminPanel/add_to_ShoppingCart/Cart_Deletion.php
+++ adminPanel/add_to_ShoppingCart/Cart_Deletion.php
@@ -13,25 +13,24 @@ include "config.php";
 
 <?php
 
-$sql_command = "SELECT  pid, uid, amount FROM add_to_ShoppingCart ";
-$sql_command2 = "SELECT  uName FROM Users ";
-$sql_command3 = "SELECT  pName FROM Products ";
+$sql_command = "SELECT  *  FROM add_to_ShoppingCart NATURAL JOIN Users NATURAL JOIN Products";
+
 
 $myresult = mysqli_query($db, $sql_command);
-$myresult2 = mysqli_query($db, $sql_command2);
-$myresult3 = mysqli_query($db, $sql_command3);
 
     while($id_rows = mysqli_fetch_assoc($myresult))
     {
-
-
         $amount = $id_rows['amount'];
 
         $pid = $id_rows['pid'];
 
         $uid = $id_rows['uid'];
 
-        echo "<option value='$pid-$uid'>". $pid . " ". $uid . " " . $amount . "</option>";
+        $pName = $id_rows['pName'];
+
+        $uName = $id_rows['uName'];
+
+        echo "<option value='$pid-$uid'>". $pName . " ". $uName . " " . $amount . "</option>";
 }
         
 
@@ -40,5 +39,3 @@ $myresult3 = mysqli_query($db, $sql_command3);
 </select>
 <button>DELETE</button>
 </form>
-
-
