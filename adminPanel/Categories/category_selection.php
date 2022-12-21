d964bb89521a403c71a0f4545ef0112a1a6579e5
hiDeniz <halilibrahimdeniz2001@gmail.com>
2022-12-21 17:19:46 +0300

    <adminpanel_final>

--- adminPanel/Categories/category_selection.php
+++ adminPanel/Categories/category_selection.php
@@ -1,27 +1,25 @@
-
-<form action="select.php" method="POST"> 
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
 <?php
 include "config.php";
 
-$sql_command = "SELECT DISTINCT aCountry FROM Addresses ORDER BY aCountry ASC";
-
+$sql_command = "SELECT DISTINCT * FROM Categories ORDER BY cid ASC";
 $myResult = mysqli_query($db,$sql_command);
 ?>
+<h1>SELECT CATEGORY</h1>
+<form action="selectCategory.php" method="POST">
+<select id= "cid "name="cid">
+<?php
 
-<center>
-  
-  <label for="category-select">Select a category:</label>
-  <select id="category-select" name="category">
-    <?php
-      // Loop through the list of categories and create an option for each one
-      while ($row = mysqli_fetch_assoc($result)) {
-        $id = $row['cid'];
-        $name = $row['cname'];
-        echo "<option value='$id'>$name</option>";
-      }
-    ?>
-  </select>
-  
-</center>
+while($id_rows = mysqli_fetch_assoc($myResult)){
+    $cName = $id_rows['cName'];
+    $cid = $id_rows['cid'];
+    echo "<option value=$cid>" . $cName  . "</option>";
+}
+
+?>
+</select>
+<button>SELECT</button>
 </form>
